ab5f269153f3435f29c03f74c0573df8d1eb3598
Валентин Алтабасов <valenjir@gmail.com>
2025-02-05 17:10:01 +0300

    fix brain-gcd

--- brain_games/scripts/games/brain_gcd.py
+++ brain_games/scripts/games/brain_gcd.py
@@ -14,21 +14,23 @@ def gcd(a, b):
 def main():
     name = welcome_user()
     game_count = 3
-    print('Find the greatest common divisor of given numbers')
+    print('Find the greatest common divisor of given numbers.')
     while game_count != 0:
         number_1 = random_value()
         number_2 = random_value()
-        game_count -= 1
         result = gcd(number_1, number_2)
         answer = prompt.integer(f'Question: {number_1} {number_2}\n'
                                 f'Your answer: ')
         if answer == result:
+            game_count -= 1
             print('Correct!')
         else:
-            return (f"'{answer}' is wrong answer ;(."
-                    f"Correct answer was '{result}'.\n"
-                    f"Let's try again, {name}!") 
-    return f'Congratulations, {name}!'    
+            game_count = -1
+            print (f"'{answer}' is wrong answer ;(."
+                   f"Correct answer was '{result}'.\n"
+                   f"Let's try again, {name}!") 
+            break
+    print(f'Congratulations, {name}!')  
 
 
 if __name__ == '__main__':
