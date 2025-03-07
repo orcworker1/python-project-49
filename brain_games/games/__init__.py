6d17087b8906dc2693654eaea1d30162356638a4
Валентин Алтабасов <valenjir@gmail.com>
2025-03-07 13:59:38 +0300

    fix init

---
+++ brain_games/games/__init__.py
@@ -0,0 +1,5 @@
+from .calc import generate_round as brain_calc
+from .even import generate_round as brain_even
+from .gcd import generate_round as brain_gcd
+from .prime import generate_round as brain_prime
+from .progression import generate_round as brain_progression
