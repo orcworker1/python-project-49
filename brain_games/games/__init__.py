7dbcf441cb91824eda522535bb2458638e4cb3f0
Валентин Алтабасов <valenjir@gmail.com>
2025-03-07 15:01:04 +0300

    fix

--- brain_games/games/__init__.py
+++ brain_games/games/__init__.py
@@ -1,7 +1,18 @@
-from brain_games.games.calc import generate_round as brain_calc, DESCRIPTION as calc_description
-from brain_games.games.even import generate_round as brain_even , DESCRIPTION as even_description
-from brain_games.games.gcd import generate_round as brain_gcd , DESCRIPTION as gcd_description
-from brain_games.games.prime import generate_round as brain_prime, DESCRIPTION as prime_description
-from brain_games.games.progression import generate_round as brain_progression , DESCRIPTION as progression_description
+from .calc import DESCRIPTION as calc_description
+from .calc import generate_round as brain_calc
+from .even import DESCRIPTION as even_description
+from .even import generate_round as brain_even
+from .gcd import DESCRIPTION as gcd_description
+from .gcd import generate_round as brain_gcd
+from .prime import DESCRIPTION as prime_description
+from .prime import generate_round as brain_prime
+from .progression import DESCRIPTION as prog_description
+from .progression import generate_round as brain_progression
 
-
+__all__ = [
+    'brain_calc', 'calc_description',
+    'brain_even', 'even_description',
+    'brain_gcd', 'gcd_description',
+    'brain_prime', 'prime_description',
+    'brain_progression', 'prog_description',
+]
